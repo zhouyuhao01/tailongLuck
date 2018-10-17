25f0c4ecce35f72d4528558e8e581ade184324fc
子冬 <joe.zyh@antfin.com>
2018-10-17 17:20:38 +0800

    init

--- src/App.js
+++ src/App.js
@@ -6,26 +6,6 @@ import Camera from './camera/Camera'
 
 class App extends Component {
 
-  getMedia() {
-    let constraints = {
-        video: {width: 500, height: 500},
-        audio: true
-    };
-    let video = document.getElementById("video");
-    let promise = navigator.mediaDevices.getUserMedia(constraints);
-    promise.then(function (MediaStream) {
-        video.srcObject = MediaStream;
-        video.play();
-    });
-  }
-
-  takePhoto() {
-    let video = document.getElementById("video");
-    let canvas = document.getElementById("canvas");
-    let ctx = canvas.getContext('2d');
-    ctx.drawImage(video, 0, 0, 500, 500);
-  }
-
   render() {
     return (
       <div >
